63ccba4f78a379a99c1baa6e9ebaebf339805266
CP <jorge.andresocampo@gmail.com>
2020-02-28 20:55:47 -0500

    lastCommit

--- burger/src/Components/Burger/Burger.js
+++ burger/src/Components/Burger/Burger.js
@@ -5,11 +5,54 @@ import classes from './Burger.css';
 import BurgerIngredient from '../BurgerIngredients/BurgerIngredient'
 
 const burger = (props) => {
+    //receiving ingredients
+    let ingredients = props.ingredients;
+    //converting object literal into a list
+    let list1 = Object.entries(ingredients);
+
+
+    let list2 = [];
+    list1.map(el => {
+        for (let x =0; x < el[1]; x++){
+            list2.push(el[0])
+        }
+
+
+    });
+
+    console.log(list2);
+
+
+
+
+
+
     return (
+
+
+
         <div className={classes.Burger}>
+
+
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {
+                list2.map(el => {
+                    return <BurgerIngredient type={el}/>
+
+                })
+
+            }
+
+
+
+
+
+
+
+
+
+
+
             <BurgerIngredient type="bread-bottom" />
 
            </div>
@@ -18,7 +61,7 @@ const burger = (props) => {
 
 
     
-}
+};
 
 
 export default burger;
--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,13 +5,33 @@ import Burger from '../../Components/Burger/Burger';
 
 
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+
+            ingredients: {
+                salad: 1,
+                bacon:1,
+                cheese: 2,
+                meat:2
+
+
+
+            }
+
+
+
+
+        }
+
+    }
 
     render() {
 
         return (
 
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls </div>
                 </Aux>
             
